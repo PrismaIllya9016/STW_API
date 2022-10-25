cb3b1f8809ac33b9673f3d6665fec4a74d16c6da
PrismaIllya9016 <malopez091@gmail.com>
2022-10-25 03:23:39 +0000

    push

--- src/main.py
+++ src/main.py
@@ -11,6 +11,8 @@ from admin import setup_admin
 from models import db, User, People, Favorite_People, Planets, Favorite_Planets, Vehicles, Favorite_Vehicles
 #from models import Person
 
+
+
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
